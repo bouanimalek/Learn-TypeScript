34e4e324cf136ede958dc440cf22943de05ae5b3
malek <malzovich@gmail.com>
2023-02-01 15:18:37 +0100

    The "unknown" and "never" Type

--- app.ts
+++ app.ts
@@ -1,25 +1,14 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+  userName = userInput;
 }
 
-function printResultOfThis(num: number): void {
-  console.log("Result: " + num);
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-printResultOfThis(add(5, 12));
-
-// let combineValues__: Function;
-let combineValues__Of: (a: number, b: number) => number;
-combineValues__Of = add;
-// combineValues__ = 5;
-// combineValues__Of= printResultOfThis;
-
-console.log(combineValues__Of(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
+generateError("An error occured!", 500);
