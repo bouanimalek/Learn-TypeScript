18b73c6a7cdeead35364c9a29ce8e229f64c2e50
malek <malzovich@gmail.com>
2023-02-01 12:55:33 +0100

    Function Types & Callbacks

--- app.ts
+++ app.ts
@@ -6,6 +6,10 @@ function printResultOfThis(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
 printResultOfThis(add(5, 12));
 
 // let combineValues__: Function;
@@ -15,3 +19,7 @@ combineValues__Of = add;
 // combineValues__Of= printResultOfThis;
 
 console.log(combineValues__Of(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
