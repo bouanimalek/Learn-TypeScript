1b6737dc4b37692ebb6449efb1e8152fd9b9bc2d
malek <malzovich@gmail.com>
2023-02-01 15:49:33 +0100

    Including & Excluding Files

--- app.ts
+++ app.ts
@@ -1,14 +1,3 @@
-let userInput: unknown;
-let userName: string;
+let userName = "Malek";
 
-userInput = 5;
-userInput = "Max";
-if (typeof userInput === "string") {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
-
-generateError("An error occured!", 500);
+console.log(userName);
