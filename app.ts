6534e55be0601c1d26182064259ef9349139d2da
malek <malzovich@gmail.com>
2023-02-01 12:46:32 +0100

    Functions as Types

--- app.ts
+++ app.ts
@@ -2,8 +2,16 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-function printResultOf(num: number): void {
+function printResultOfThis(num: number): void {
   console.log("Result: " + num);
 }
 
-printResultOf(add(5, 12));
+printResultOfThis(add(5, 12));
+
+// let combineValues__: Function;
+let combineValues__Of: (a: number, b: number) => number;
+combineValues__Of = add;
+// combineValues__ = 5;
+// combineValues__Of= printResultOfThis;
+
+console.log(combineValues__Of(8, 8));
