dd7474efec90a552ae4ec014817146a6be72012f
malek <malzovich@gmail.com>
2023-02-01 17:24:30 +0100

    configuring tsconfig.json

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+const button = document.querySelector("button")!;
+const clickHandler = (message: string) => {
+  console.log("Clicked! " + message);
+};
+button.addEventListener("click", clickHandler.bind(null, "yes"));
